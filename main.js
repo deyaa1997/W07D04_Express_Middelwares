4feff940b71c8631a0932e1d77e9101b0e6e4294
deyaa1997 <deyah.mosa@gmail.com>
2021-05-13 02:06:27 +0300

    Practice 4

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const port = 3000;
 const userHandle = express.Router();
 const productsHandle = express.Router();
 const users = ["John", "Mark"];
+const products = ["keyboard", "mouse"];
 
 /*const logUsers = (req, res, next) => {
     console.log(users);
@@ -12,24 +13,19 @@ const users = ["John", "Mark"];
   };
 */
 
-const logMethod =
-  ("/users",
-  (req, res, next) => {
-    console.log(req.method);
-    next();
-  });
+const logMethod = (req, res, next) => {
+  console.log(req.method);
+  next();
+};
 
 userHandle.use((req, res, next) => {
   console.log(users);
   next();
 });
 
-
-
 app.use(express.json());
 // app.use(logUsers);
-app.use(logMethod);
-
+app.use("/users", logMethod);
 
 if (users.length > 0) {
   app.get("/users", (req, res, next) => {
@@ -45,16 +41,35 @@ if (users.length > 0) {
 }
 
 userHandle.use("/create", (req, res, next) => {
-    if (req.body.name) {
-      console.log(req.body.name);
-      next();
-    }
-  });
+  if (req.body.name) {
+    console.log(req.body.name);
+    next();
+  }
+});
 
-  
 userHandle.post("/create", (req, res) => {
   users.push(req.body.name);
   res.json(users);
+});
+
+productsHandle.post("/update", (req, res) => {
+  // suppose update value and new value come from body like this :-
+  /* {
+        "update" : "keyboard",
+    "newValue" : "ss"}
+    */
+  let status = false;
+  for (let x = 0; x < products.length; x++) {
+    if (req.body.update === products[x]) {
+      products[x] = req.body.newValue;
+      status = true;
+      res.json("Your products Update");
+    }
+  }
+  if (status === false) {
+    res.json("You cant update this items");
+  }
+  console.log(products);
 });
 
 app.use((err, req, res, next) => {
