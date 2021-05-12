7a4b52203aed65a360111e56a424a7338d55c59b
deyaa1997 <deyah.mosa@gmail.com>
2021-05-12 12:20:21 +0300

    Finsish All Pulse Check

--- main.js
+++ main.js
@@ -3,20 +3,38 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-const users = ["John", "Mark"];
-const logUsers = (req , res , next) => {
-    console.log(users)
-    next()
-}
+const users = [];
+const logUsers = (req, res, next) => {
+  console.log(users);
+  next();
+};
 
-
+const logMethod =
+  ("/users",
+  (req, res, next) => {
+    console.log(req.method);
+    next();
+  });
 
 app.use(express.json());
-app.use(logUsers)
+app.use(logUsers);
+app.use(logMethod);
 
+if (users.length > 0) {
+  app.get("/users", (req, res, next) => {
+    res.json(users);
+  });
+} else {
+  app.get("/users", (req, res, next) => {
+    // create a new error
+    const err = new Error("No users");
+    // pass it to next, we only pass values to `next` when we want to call the error handling middleware
+    next(err);
+  });
+}
 
-app.get("/users", (req, res, next) => {
-  res.json(users);
+app.use((err, req, res, next) => {
+  res.json(err.message);
 });
 
 app.listen(port, () => {
